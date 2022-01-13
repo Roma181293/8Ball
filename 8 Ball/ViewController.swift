1cbc897491edbbe1d1a3e0c5ad000a4a8db0e432
Roman Topchii <roma.topchiy@me.com>
2022-01-13 18:30:10 +0200

    Initial Commit

---
+++ 8 Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8 Ball
+//
+//  Created by Roman Topchii on 13.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
